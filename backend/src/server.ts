5be99ad121ba601f446eb639468d6b039e7f6958
panuwat thammabut <panuwat.tha@kkumail.com>
2025-07-23 23:40:33 +0700

    .

---
+++ backend/src/server.ts
@@ -0,0 +1,33 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+import cors from 'cors';
+import connectDB from './utils/database';
+import authRoutes from './routes/authRoutes';
+import vehicleRoutes from './routes/vehicleRoutes';
+import driverRoutes from './routes/driverRoutes';
+
+const app = express();
+const PORT: number = Number(process.env.PORT) || 5000;
+
+connectDB();
+
+app.use(cors({
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+app.use(express.json());
+app.use('/api',authRoutes);
+app.use('/api', vehicleRoutes);
+app.use('/api', driverRoutes);
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
+
